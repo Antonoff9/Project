8dd2fa68deb8b6686e4c52a682db06700c7a6147
Antonoff9 <152692231+Antonoff9@users.noreply.github.com>
2024-05-01 14:26:16 +0300

    Update ebc.py

--- Python/ebc.py
+++ Python/ebc.py
@@ -3,16 +3,16 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver import ChromeOptions
 
-# driver = webdriver.Firefox(executable_path='C:\\Users\\ASUS\\Desktop\\geckodriver.exe')
-# driver.get('http://www.google.com/')
-# time.sleep(5)
-# search_box = driver.find_element_by_name('q')
-# search_box.send_keys('FirefoxDriver')
-# search_box.submit()
-# time.sleep(5)
-# driver.quit()
+driver = webdriver.Firefox(executable_path='C:\\Users\\ASUS\\Desktop\\geckodriver.exe')
+driver.get('http://www.google.com/')
+time.sleep(5)
+search_box = driver.find_element_by_name('q')
+search_box.send_keys('FirefoxDriver')
+search_box.submit()
+time.sleep(5)
+driver.quit()
 
-# link = "https://xn-----elcahffngcif9bjk1b7a3e8dh.xn--p1ai/"
+link = "https://xn-----elcahffngcif9bjk1b7a3e8dh.xn--p1ai/"
 link = "единый-визовый-центр.рф"
 options = ChromeOptions()
 options.binary_location = "C:\\chromedriver\\chromedriver.exe"
